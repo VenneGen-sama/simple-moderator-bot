ad702b0cbb82debccc6f7a8c4e1170edd244e213
VenneGen-sama <itsvinc97@gmail.com>
2021-06-13 02:47:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+const { Client, Collection } = require("discord.js");
+const { config } = require("dotenv");
+const {prefix, token } = require("./config.json")
+
+const client = new Client({
+    disableEveryone: true
+})
+
+// Collections
+client.commands = new Collection();
+client.aliases = new Collection();
+
+
+ // Run the command loader
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
+
+    client.user.setActivity("Bot khusus eksperimen") 
+})
+
+client.on("message", async message => {
+   
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // If message.member is uncached, cache it.
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    // Get the command
+    let command = client.commands.get(cmd);
+    // If none is found, try to find it by alias
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    // If a command is finally found, run the command
+    if (command) 
+        command.run(client, message, args);
+});
+
+client.login(token);
